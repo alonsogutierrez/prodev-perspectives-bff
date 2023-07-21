d16cf1a3d6644ea6d0cf3caeb0b7d672dc47fbe8
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2023-07-20 23:00:50 -0400

    feat(app): refactor to get app without app config in the same file, modify nodejs version to 14

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import healthRouter from './api/health/insfrastructure/route';
+import postsRouter from './api/posts/insfrastructure/routes';
+import usersRouter from './api/users/insfrastructure/routes';
+
+import MongoDbClient from './api/shared/insfrastructure/mongoDbClient';
+
+const app = express();
+
+try {
+  const mongoDBRepo: MongoDbClient = new MongoDbClient();
+  mongoDBRepo.connectDB();
+} catch (err: any) {
+  throw new Error(`Error connecting with MongoDB: ${err?.message}`);
+}
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Routers
+app.use(healthRouter);
+app.use(postsRouter);
+app.use(usersRouter);
+
+export default app;
